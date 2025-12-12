b1fef6d082e9fe993f726a80a46c1aef5787d6de
Dipak Zende <114248721+dipak307@users.noreply.github.com>
2025-12-13 00:54:20 +0530

    Update CORS settings for Socket.IO server

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,9 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://vocal-pastelito-0f9df4.netlify.app"],
     credentials:true,
-    methods:['POST','GET','UPDATE','DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 })
 
