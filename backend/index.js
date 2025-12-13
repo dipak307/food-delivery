1a9489e5a2918d6ec817e3d144d690c14a770ae6
Dipak Zende <114248721+dipak307@users.noreply.github.com>
2025-12-13 10:32:47 +0530

    Update CORS origin for deployment

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin:["http://localhost:5173","https://vocal-pastelito-0f9df4.netlify.app"],
+    origin:["http://localhost:5173","https://dip-food-delivery.netlify.app"],
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
@@ -31,7 +31,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:["http://localhost:5173","https://vocal-pastelito-0f9df4.netlify.app"],
+    origin:["http://localhost:5173","https://dip-food-delivery.netlify.app"],
     credentials:true
 }))
 app.use(express.json())
